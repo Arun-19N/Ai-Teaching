28cca8f74437a1fbd2f6f736315daa8b9e236d4e
arun-19n <nuraruran32@gmail.com>
2026-06-03 22:19:59 +0530

    Make navbar 100% width

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,7 +62,7 @@ export default function Navbar() {
           scrolled ? 'glass shadow-md' : 'bg-transparent'
         }`}
       >
-        <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <nav className="w-full px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             {/* Logo */}
             <Link to="/" className="flex items-center gap-2.5 group">
